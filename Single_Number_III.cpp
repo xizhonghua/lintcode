126754bb9ecd3cc0fbce6f38c0fb48617db48b4e
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-21 22:28:17 -0500

    Update Single_Number_III.cpp

--- Single_Number_III.cpp
+++ Single_Number_III.cpp
@@ -1,3 +1,11 @@
+// Given 2*n + 2 numbers, every numbers occurs twice except two, find them.
+
+// Example
+// Given [1,2,2,3,4,4,5,3] return 1 and 5
+
+// Challenge
+// O(n) time, O(1) extra space.
+
 class Solution {
 public:
     /**
