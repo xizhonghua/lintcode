bd46805355b63981e508e947739c4e05e2280358
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-23 14:38:02 -0500

    Create 2Sum.cpp

---
+++ 2Sum.cpp
@@ -0,0 +1,36 @@
+// Time = O(n)
+// Space = O(n)
+class Solution {
+public:
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1+1, index2+1] (index1 < index2)
+     */
+    vector<int> twoSum(vector<int> &nums, int target) {
+        // write your code here
+        
+        unordered_map<int,int> m;
+        
+        for(auto i=0;i<nums.size();++i)
+            m[nums[i]]=i+1;
+        
+        
+        for(auto i=0;i<nums.size();++i)
+        {
+            const auto n = nums[i];
+            const auto left = target - n;
+            if(!m.count(left)) continue;
+            
+            if(n==left && i != m[left] || n!=left)
+            {
+                auto l = min(m[left], i+1);
+                auto h = max(m[left], i+1);
+                return vector<int>{l, h};
+            }
+        }
+        
+        return vector<int>{-1,-1};
+    }
+};
+
