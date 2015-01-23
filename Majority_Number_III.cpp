6816c2fbcffdf321eeb4690122eb86260a1bb66b
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-22 23:18:44 -0500

    Update Majority_Number_III.cpp

--- Majority_Number_III.cpp
+++ Majority_Number_III.cpp
@@ -11,24 +11,21 @@ public:
         vector<int> c(k,1);
         map<int,int> m;
         
-        int index = 0;
+        auto index = 0;
         
         for(auto n : nums)
         {
-            if(m.count(n))
-                c[m[n]]++;
-            else
-            {
-                    c[index]--;
-                    if(c[index]==0)
-                    {
-                        m.erase(s[index]);
-                        m[n] = index;
-                        s[index] = n;
-                        c[index] = 1;
-                    }
-                    index++;
-                    if(index>k) index=0;
+            if (m.count(n)) {
+                ++c[m[n]];
+            } else {
+                if(--c[index]==0)
+                {
+                    m.erase(s[index]);
+                    m[n] = index;
+                    s[index] = n;
+                    c[index] = 1;
+                }
+                index = (++index)%k;
             }
         }
         
@@ -37,3 +34,4 @@ public:
         return s[it-c.begin()];
     }
 };
+
