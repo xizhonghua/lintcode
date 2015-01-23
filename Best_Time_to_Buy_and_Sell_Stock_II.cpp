317c52008f7888abbf4203d85f21211a96f69e05
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-23 14:41:44 -0500

    Create Best_Time_to_Buy_and_Sell_Stock_II.cpp

---
+++ Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    int maxProfit(vector<int> &prices) {
+        // write your code here
+        int profit = 0;
+        for(int i=1;i<prices.size();++i)
+            if(prices[i] > prices[i-1]) profit += (prices[i] - prices[i-1]);
+        return profit;
+    }
+};
