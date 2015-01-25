0571757d2bf7e9f1fc55a6fe72a47dac8361075e
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-24 22:34:30 -0500

    Create Sort_Colors_II.cpp

---
+++ Sort_Colors_II.cpp
@@ -0,0 +1,29 @@
+class Solution{
+public:
+    /**
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */    
+    void sortColors2(vector<int> &colors, int k) {
+        // write your code here
+        auto r =  colors.size()-1;
+        auto e = r;
+        for(auto i=0;i<k;++i)
+            e = partition(colors, 0, e, k-i-1);
+    }
+    
+private:
+    int partition(vector<int> &nums, int l, int r, int pivot)
+    {
+        int i=l, j=r;
+        while(i<=j)
+        {
+            while(i<r && nums[i] <= pivot) ++i;
+            while(j>0 && nums[j] > pivot) --j;
+            if(i<=j) swap(nums[i++], nums[j--]);
+        }
+        
+        return i-1;
+    }
+};
