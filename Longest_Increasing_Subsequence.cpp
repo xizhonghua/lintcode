f296e7c4a358ada52b1421a0f2c63eb81bece161
Zhonghua Xi <xizhonghua@gmail.com>
2015-01-25 23:19:53 -0500

    Create Longest_Increasing_Subsequence.cpp

---
+++ Longest_Increasing_Subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    /**
+     * @param nums: The integer array
+     * @return: The length of LIS (longest increasing subsequence)
+     */
+    int longestIncreasingSubsequence(vector<int> nums) {
+        // write your code here
+        auto size = nums.size();
+        if (size==0) return 0;
+        vector<int> f(size, 1);
+        for(auto i=0;i<size;++i)
+            for(auto j=0;j<i;++j)
+                if(nums[i]>=nums[j])
+                    f[i] = max(f[i], f[j]+1);
+                
+        return *std::max_element(f.begin(), f.end());
+    }
+};
+
