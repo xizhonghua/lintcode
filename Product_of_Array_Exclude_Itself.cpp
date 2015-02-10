9d9a815606e01190e7772fca50fd950791713fdc
Zhonghua Xi <xizhonghua@gmail.com>
2015-02-10 10:12:10 -0500

    Update Product_of_Array_Exclude_Itself.cpp

--- Product_of_Array_Exclude_Itself.cpp
+++ Product_of_Array_Exclude_Itself.cpp
@@ -9,19 +9,19 @@ public:
         const auto size = nums.size();
         if(size==1) return {};
         
-        vector<vector<long long>> f(size+1, vector<long long>(size+1, 1));
+        vector<vector<long long>> f(size, vector<long long>(size, 1));
         
         // f[i][j] == A[i] * A[i+1] * ... * A[j]
-        for(auto i=1;i<=size;++i)
-            for(auto j=i;j<=size;++j)
-                f[i][j] = f[i][j-1]*nums[j-1];
+        for(auto i=0;i<size;++i)
+            for(auto j=i;j<size;++j)
+                f[i][j] = (j==0 ? 1 : f[i][j-1])*nums[j];
         
         vector<long long> b(size, 1);
         for(auto i=0;i<size;++i)
         {
-            if(i==0) b[i] = f[2][size];
-            else if(i==size-1) b[i] = f[1][size-1];
-            else b[i] = f[1][i] * f[i+2][size];
+            if(i==0) b[i] = f[1][size-1];
+            else if(i==size-1) b[i] = f[0][size-2];
+            else b[i] = f[0][i-1] * f[i+1][size-1];
         }
         
         return b;
