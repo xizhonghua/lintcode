ae46924340fda4d469f7d6f14d772ab946b87323
Zhonghua Xi <xizhonghua@gmail.com>
2015-12-21 13:11:37 -0500

    Update Valid-Binary-Search-Tree.cpp

--- Valid-Binary-Search-Tree.cpp
+++ Valid-Binary-Search-Tree.cpp
@@ -23,16 +23,10 @@ public:
     bool isValidBST(TreeNode* root, long long max_val, long long min_val) {
         if(!root) return true;
         if(root->val >= max_val || root->val <= min_val) return false;
-        bool fl = true;
-        if(root->left) {
-            if(root->val <= root->left->val) return false;
-            fl = isValidBST(root->left, root->val, min_val);
-        }
-        bool fr = true; 
-        if(root->right) {
-            if(root->val >= root->right->val) return false;
-            fr = isValidBST(root->right, max_val, root->val);
-        }
-        return fl && fr;
+        
+        if(!isValidBST(root->left, root->val, min_val)) return false;
+        if(!isValidBST(root->right, max_val, root->val)) return false;
+        
+        return true;
     }
 };
